19bfb724f49854de7c03c3e27d41fb1b557f273e
Vitakachun <kachunvita@gmail.com>
2020-11-13 12:12:11 -0600

    add more staff to our app

--- src/Vita/Task002.java
+++ src/Vita/Task002.java
@@ -4,6 +4,7 @@ public class Task002 {
     public static void main(String[] args) {
         System.out.println("How are you today?");
         System.out.println("Today we wrote awesome code");
+        System.out.println("We are creating nice app");
 
     }
 }
