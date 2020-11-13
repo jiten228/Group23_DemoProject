189521ed0ca75f35192b5fdccd98da214a47986f
ChristinaDT <74195190+ChristinaDT@users.noreply.github.com>
2020-11-13 12:11:39 -0600

    New line added task002 for G23

--- src/ChristinaDT/Task002.java
+++ src/ChristinaDT/Task002.java
@@ -3,6 +3,7 @@ package ChristinaDT;
 public class Task002 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Create task 2");
 
     }
 }
