c1e794cc1f63aca906754111b75c005ba5d97cb1
ChristinaDT <74195190+ChristinaDT@users.noreply.github.com>
2020-11-13 11:58:24 -0600

    added a 2nd line task001 for G23

--- src/ChristinaDT/Task001.java
+++ src/ChristinaDT/Task001.java
@@ -4,5 +4,6 @@ public class Task001 {
     public static void main(String[] args) {
         System.out.println("Hello Group 23");
         //write a new code
+        System.out.println("Having fun writing codes!");
     }
 }
