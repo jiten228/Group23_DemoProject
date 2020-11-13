f67fc5c7835c65ec038aa26b7f276ed1dfa804bf
ChristinaDT <74195190+ChristinaDT@users.noreply.github.com>
2020-11-13 12:11:59 -0600

    2nd line added task002 for G23

--- src/ChristinaDT/Task002.java
+++ src/ChristinaDT/Task002.java
@@ -4,6 +4,7 @@ public class Task002 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Create task 2");
+        System.out.println(" here we go again");
 
     }
 }
