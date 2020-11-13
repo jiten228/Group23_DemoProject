9ec6c3f06d4c03447b5e6f7a7416aa772736183a
Vitakachun <kachunvita@gmail.com>
2020-11-13 12:11:37 -0600

    added awesome code

--- src/Vita/Task002.java
+++ src/Vita/Task002.java
@@ -3,6 +3,7 @@ package Vita;
 public class Task002 {
     public static void main(String[] args) {
         System.out.println("How are you today?");
+        System.out.println("Today we wrote awesome code");
 
     }
 }
