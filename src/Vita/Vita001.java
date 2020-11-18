e2b0ab29215a77c8e53835cbf54c4aa26593ab2d
Vitakachun <kachunvita@gmail.com>
2020-11-17 23:44:31 -0600

    added more stuff

--- src/Vita/Vita001.java
+++ src/Vita/Vita001.java
@@ -4,5 +4,6 @@ public class Vita001 {
     public static void main(String[] args) {
         System.out.println("Good evening, everyone!");
         System.out.println("How are you?");
+        System.out.println("Have a good night!");
     }
 }
